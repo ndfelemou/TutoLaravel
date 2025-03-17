5bb4549192979922422aba8b5f9823a124e174c2
Nyankoye Daniel FELEMOU <127888791+ndfelemou@users.noreply.github.com>
2025-03-17 21:50:41 +0100

    02: Notion d'Object Relational Mapping (ORM) Eloquent

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,18 +21,18 @@ Route::get('/', function () {
 
 Route::prefix('/blog')->name('blog.')->group(function () {
     Route::get('/', function (Request $request) {
-        return [
-            "link" => route('blog.show', ['slug' => 'article', 'id' => 13]),
-        ];
+        return Post::paginate(25);
     })->name('index');
 
 
-    Route::get('/{slug}/{id}', function (string $slug, string $id, Request $request) {
-        return [
-            "slug" => $slug,
-            "id" => $id,
-            "name" => $request->input('name'),
-        ];
+    Route::get('/{slug}-{id}', function (string $slug, string $id, Request $request) {
+        $post = Post::findOrFail($id);
+
+        if ($post->slug !== $slug) {
+            return to_route('blog.show', ['slug' => $post->slug, 'id' => $post->id]);
+        }
+
+        return $post;
     })->where([
         'id' => '[1-9]+',
         'slug' => '[a-z0-9\-]+'
